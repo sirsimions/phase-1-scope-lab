557f250caecb7f9f487edb470ca0cda203963bf2
Simion <simion.okola@student.moringaschool.com>
2022-08-20 15:32:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer ='not bob'
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer ='maybe bob';
+    return bestCustomer;
+}
+const leastFavoriteCustomer = 'value1';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'value2';
+    return leastFavoriteCustomer;
+}
